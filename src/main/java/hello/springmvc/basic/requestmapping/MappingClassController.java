8e2c46b64b41d54536fe2caeb31c0393b849a5e4
KORYEcan <philo0401@naver.com>
2023-04-19 02:09:25 +0900

    50%

---
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,36 @@
+package hello.springmvc.basic.requestmapping;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+@GetMapping
+  public String user(){
+    return "get users";
+  }
+
+ @PostMapping
+  public String addUser(){
+    return "post users";
+  }
+
+   @GetMapping("/{userId}")
+  public String findUser(@PathVariable String userId){
+  return "get userId="+ userId;
+  }
+
+   @PatchMapping("/{userId}")
+  public String updateUser(@PathVariable String userId){
+  return "update userId="+ userId;
+  }
+
+   @DeleteMapping("/{userId}")
+  public String DeleteUser(@PathVariable String userId){
+  return "Delete userId="+ userId;
+  }
+
+
+}
